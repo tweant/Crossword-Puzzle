c3a0a887f2b12fa6ec1804b117d7d6210cd1acd5
Marcin Kraska <m.kraska@student.mini.pw.edu.pl>
2017-11-25 13:16:48 +0100

    Empty project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace crossword_puzzle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
